b5aaf5f6d238adad01484e55fc198eaa46ba0225
Ryan Catlin <ryan.catlin@gmail.com>
2014-08-19 19:44:14 -0400

    Switch to routing annotations

--- src/MyProject/Bundle/MainBundle/Controller/MainController.php
+++ src/MyProject/Bundle/MainBundle/Controller/MainController.php
@@ -4,9 +4,13 @@ namespace MyProject\Bundle\MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends Controller
 {
+    /**
+     * @Route("/", name="index")
+     */
     public function indexAction()
     {
         return new Response('Hello, galaxy!', 200);
