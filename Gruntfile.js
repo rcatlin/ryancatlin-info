da7496a88ed31c191ef74de186014cfbb5525f2a
Ryan Catlin <ryan.catlin@gmail.com>
2014-08-28 00:11:49 -0400

    Gruntify assets

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        bowercopy: {
+            options: {
+                srcPrefix: 'bower_components',
+                destPrefix: 'web/assets'
+            },
+            scripts: {
+                files: {
+                    'js/jquery.min.js': 'jquery/dist/jquery.min.js',
+                    'js/bootstrap.min.js': 'bootstrap/dist/js/bootstrap.min.js'
+                }
+            },
+            stylesheets: {
+                files: {
+                    'css/bootstrap.min.css': 'bootstrap/dist/css/bootstrap.min.css',
+                    'css/bootstrap-theme.min.css': 'bootstrap/dist/css/bootstrap-theme.min.css'
+                }
+            },
+            fonts: {
+                files: {
+                    'fonts': 'bootstrap/dist/fonts'
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-bowercopy');
+    grunt.registerTask('default', ['bowercopy']);
+}
