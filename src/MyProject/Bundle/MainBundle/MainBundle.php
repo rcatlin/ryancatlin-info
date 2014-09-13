04a163d353c63d29a61e84befdfa78bdc05f54b8
Ryan Catlin <ryan.catlin@gmail.com>
2014-09-13 16:24:08 -0400

    Hook up AWS SDK v2 and custom s3 dump command

--- src/MyProject/Bundle/MainBundle/MainBundle.php
+++ src/MyProject/Bundle/MainBundle/MainBundle.php
@@ -6,9 +6,4 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class MainBundle extends Bundle
 {
-    public function boot()
-    {
-        // Register Amazon S3 Client Stream Wrapper
-        $this->container->get('aws_s3.client')->registerStreamWrapper();
-    }
 }
