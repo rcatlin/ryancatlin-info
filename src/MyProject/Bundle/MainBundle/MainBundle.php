b2d7f4f5353fcebe8ad418abad8c3896af131227
Ryan Catlin <ryan.catlin@gmail.com>
2014-09-12 22:47:45 -0400

    register s3 stream wrapper in main bundle

--- src/MyProject/Bundle/MainBundle/MainBundle.php
+++ src/MyProject/Bundle/MainBundle/MainBundle.php
@@ -6,4 +6,9 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class MainBundle extends Bundle
 {
+    public function boot()
+    {
+        // Register Amazon S3 Client Stream Wrapper
+        $this->container->get('aws_s3.client')->registerStreamWrapper();
+    }
 }
