c4a51c3c1a637df73c061f519722d28e8368300c
Ryan Catlin <ryan.catlin@gmail.com>
2015-10-03 19:09:04 -0400

    Update tests for Piston upgrade

--- tests/Controller/MainControllerTest.php
+++ tests/Controller/MainControllerTest.php
@@ -5,6 +5,7 @@ namespace RCatlin\Blog\Tests\Controller;
 use RCatlin\Blog\Controller\MainController;
 use Refinery29\Piston\Http\Request;
 use Refinery29\Piston\Http\Response;
+use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 
 class MainControllerTest extends \PHPUnit_Framework_TestCase
 {
@@ -12,8 +13,13 @@ class MainControllerTest extends \PHPUnit_Framework_TestCase
     {
         $controller = new MainController();
 
-        $response = $controller->index(new Request(), new Response());
+        $response = $controller->index(
+            new Request(),
+            new Response(
+                new SymfonyResponse()
+            )
+        );
 
-        $this->assertSame('Hello, world.', $response->getContent());
+        $this->assertSame('{"result":{"message":"Hello, world."}}', $response->getContent());
     }
 }
