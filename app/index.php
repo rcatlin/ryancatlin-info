fc4232d1574b269f9965670576c838d11ebd4734
Ryan Catlin <ryan.catlin@gmail.com>
2015-10-17 13:50:18 -0400

    Support new version of Piston and fix tests

--- app/index.php
+++ app/index.php
@@ -1,17 +1,18 @@
 <?php
 
 /* @var Container */
-$container = require __DIR__ . '/../bootstrap.php';
+$container = require __DIR__.'/../bootstrap.php';
 
 use League\Container\Container;
 use Refinery29\Piston\Piston;
+use RCatlin\Blog\Middleware;
 
-$whoops = new \Whoops\Run;
-$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+$whoops = new \Whoops\Run();
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
 $whoops->register();
 
 $app = new Piston($container);
 
-$app->addDecorator(new \RCatlin\Blog\Decorator\RouteDecorator($app));
+$app->addMiddleware(new Middleware\Route\Main());
 
 $app->launch();
