2b865b4961f3c9cedba74cb58b5c978aa7ae9057
Ryan Catlin <ryan.catlin@gmail.com>
2015-11-01 12:46:30 -0500

    Exit integration test script if starting local server fails

--- script/integration.sh
+++ script/integration.sh
@@ -6,6 +6,12 @@ php -S localhost:8000 -t app& &> /dev/null
 
 # Get PID of PHP Server
 PHP_PID=$!
+PHP_RETURN_CODE=$?
+
+if [ $PHP_RETURN_CODE -ne 0 ]; then
+    echo "Error occurred starting PHP Server:" $PHP_RETURN_CODE
+    exit $PHP_RETURN_CODE
+fi
 
 echo "Started local PHP server with Process ID: " + $PHP_PID
 
