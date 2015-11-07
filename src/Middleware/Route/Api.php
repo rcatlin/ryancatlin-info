1c2b0ad130143e195ad346cdcfaa51a6c538b3e4
Ryan Catlin <ryan.catlin@gmail.com>
2015-11-07 14:24:13 -0500

    Article Update Route

--- src/Middleware/Route/Api.php
+++ src/Middleware/Route/Api.php
@@ -29,6 +29,7 @@ class Api implements StageInterface
             $group->post('articles', Controller\Api\ArticleCreateController::class . '::create');
             $group->delete('articles/{id:number}', Controller\Api\ArticleDeleteController::class . '::delete');
             $group->patch('articles/{id:number}', Controller\Api\ArticleUpdateController::class . '::partialUpdate');
+            $group->put('articles/{id:number}', Controller\Api\ArticleUpdateController::class . '::update');
 
             $group->get('tags/{id:number}', Controller\Api\TagGetController::class . '::get');
             $group->post('tags', Controller\Api\TagCreateController::class . '::create');
