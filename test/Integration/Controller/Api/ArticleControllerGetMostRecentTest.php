ba10cf81929df931539d67339595d13cae1077d0
Ryan Catlin <ryan.catlin@gmail.com>
2015-11-07 18:01:49 -0500

    Article Get Most Recent Test Uses Factory Muffin trait; fix created date times

--- test/Integration/Controller/Api/ArticleControllerGetMostRecentTest.php
+++ test/Integration/Controller/Api/ArticleControllerGetMostRecentTest.php
@@ -7,43 +7,28 @@ use RCatlin\Blog\Entity;
 use RCatlin\Blog\Test\CreatesGuzzleStream;
 use RCatlin\Blog\Test\HasFaker;
 use RCatlin\Blog\Test\Integration\AbstractIntegrationTest;
+use RCatlin\Blog\Test\LoadsFactoryMuffinFactories;
 use RCatlin\Blog\Test\ReadsResponseContent;
 
 class ArticleControllerGetMostRecentTest extends AbstractIntegrationTest
 {
     use CreatesGuzzleStream;
     use HasFaker;
+    use LoadsFactoryMuffinFactories;
     use ReadsResponseContent;
-
-    public static function setUpBeforeClass()
-    {
-        parent::setUpBeforeClass();
-
-        FactoryMuffin::loadFactories(
-            __DIR__ . '/../../../Factory'
-        );
-
-        $entityManager = self::$entityManager;
-        FactoryMuffin::setCustomSaver(function ($object) use ($entityManager) {
-            $entityManager->persist($object);
-            $entityManager->flush($object);
-
-            return true;
-        });
-    }
 
     public function testGetMostRecent()
     {
         /** @var Entity\Article $article0 */
         $article0 = FactoryMuffin::create(Entity\Article::class, [
             'active' => true,
-            'createdAt' => 'datetime|-1 day',
+            'createdAt' => 'dateTimeBetween|-3 days;-2 days',
         ]);
 
         /** @var Entity\Article $article1 */
         $article1 = FactoryMuffin::create(Entity\Article::class, [
             'active' => true,
-            'createdAt' => 'datetime|now',
+            'createdAt' => 'dateTimeBetween|-1 day;now',
         ]);
 
         $response = $this->client->get('/api/articles/mostrecent');
@@ -53,17 +38,5 @@ class ArticleControllerGetMostRecentTest extends AbstractIntegrationTest
         $content = json_decode($this->readResponse($response), true);
 
         $this->assertSame($article1->getId(), $content['result']['data']['id']);
-    }
-
-    public static function tearDownBeforeClass()
-    {
-        $entityManager = self::$entityManager;
-        FactoryMuffin::setCustomDeleter(function ($object) use ($entityManager) {
-            $entityManager->remove($object);
-            $entityManager->flush();
-        });
-        FactoryMuffin::deleteSaved();
-
-        parent::tearDownAfterClass();
     }
 }
