808ffab2403360d8e3b447f2d54293cea179daa0
Ryan Catlin <ryan.catlin@gmail.com>
2015-11-15 16:13:22 -0500

    Alphabetize route methods in Middleware

--- src/Middleware/Route/Api.php
+++ src/Middleware/Route/Api.php
@@ -25,17 +25,19 @@ class Api implements StageInterface
         $subject->group('api', function (RouteGroup $group) {
             $group->get('status', Controller\Api\StatusController::class . '::get');
 
-            $group->get('articles/{id:number}', Controller\Api\ArticleGetController::class . '::get');
-            $group->post('articles', Controller\Api\ArticleCreateController::class . '::create');
+            // Articles
             $group->delete('articles/{id:number}', Controller\Api\ArticleDeleteController::class . '::delete');
-            $group->patch('articles/{id:number}', Controller\Api\ArticleUpdateController::class . '::partialUpdate');
-            $group->put('articles/{id:number}', Controller\Api\ArticleUpdateController::class . '::update');
+            $group->get('articles/{id:number}', Controller\Api\ArticleGetController::class . '::get');
             $group->get('articles/mostrecent', Controller\Api\ArticleGetController::class . '::getMostRecent');
             $group->get('articles/tag/{name:word}', Controller\Api\ArticleGetController::class . '::getByTag');
+            $group->patch('articles/{id:number}', Controller\Api\ArticleUpdateController::class . '::partialUpdate');
+            $group->post('articles', Controller\Api\ArticleCreateController::class . '::create');
+            $group->put('articles/{id:number}', Controller\Api\ArticleUpdateController::class . '::update');
 
+            // Tags
+            $group->delete('tags/{id:number}', Controller\Api\TagDeleteController::class . '::delete');
             $group->get('tags/{id:number}', Controller\Api\TagGetController::class . '::get');
             $group->post('tags', Controller\Api\TagCreateController::class . '::create');
-            $group->delete('tags/{id:number}', Controller\Api\TagDeleteController::class . '::delete');
         });
 
         return $payload;
