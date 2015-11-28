cd9e0f3a431cd1c1b3893192693d943d5c8274dd
Ryan Catlin <ryan.catlin@gmail.com>
2015-11-28 18:32:33 -0500

    Index loads while not granted admin roles; change resource paths

--- web/app.js
+++ web/app.js
@@ -13,17 +13,25 @@ app.use('/static', express.static('resources'));
 // Setup Twig
 var twig = require('twig');
 twig.extendFunction('is_granted', function (role_name) {
-    return true;
+    return false;
 });
 app.set('twig options', {
     strict_variables: false
 })
 app.set('view engine', twig.__express);
 
-// Routing
+// Main Routes
 app.get('/', 'index', function (request, response) {
     response.render('index.html.twig', {articles: []});
-})
+});
+app.get('/login', 'fos_user_security_logout', function (request, response) { response.send('TODO'); });
+app.post('/login', 'fos_user_security_check', function (request, response) { response.send('TODO'); });
+app.get('/about', 'about', function (request, response) { response.send('TODO'); });
+
+// Article Routes
+app.get('/article/:slug', 'article', function (request, response) { response.send('TODO'); });
+app.get('/articles', 'article_list', function (request, response) { response.send('TODO'); });
+app.get('/tag/:name', 'articles_by_tag', function (request, response) { response.send('TODO'); });
 
 // Init Server and Listen
 var server = app.listen(8080, function () {
