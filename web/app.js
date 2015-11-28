3cdae8e871784e4d2ac5d7281129d0ec2d516549
Ryan Catlin <ryan.catlin@gmail.com>
2015-11-28 18:11:30 -0500

    Require named-routes and add twig is_granted method to express app

--- web/app.js
+++ web/app.js
@@ -1,16 +1,25 @@
 var express = require('express');
-
 var app = express();
+
+// Setup Named Routes
+var Router = require('named-routes');
+var router = new Router();
+router.extendExpress(app);
+router.registerAppHelpers(app);
 
 app.use('/static', express.static('resources'));
 
+var twig = require('twig');
+twig.extendFunction('is_granted', function (role_name) {
+    return true;
+});
 app.set('twig options', {
     strict_variables: false
 })
-app.set('view engine', 'twig');
+app.set('view engine', twig.__express);
 
-app.get('/', function (request, response) {
-    response.render('index', {message: 'Hello, world.'});
+app.get('/', 'index', function (request, response) {
+    response.render('index.html.twig', {articles: []});
 })
 
 
