eb2243d01e0c84fce882261d08c7fba94808a01f
Ryan Catlin <ryan.catlin@gmail.com>
2017-08-19 18:21:04 -0400

    Created Article and Tag models; added api to installed apps

--- Api/api/settings.py
+++ Api/api/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'app.apps.AppConfig',
     'graphene_django',
     'django.contrib.admin',
     'django.contrib.auth',
--- Api/app/models.py
+++ Api/app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    slug = models.CharField(max_length=100,
+                            unique=True)
+    title = models.CharField(max_length=255)
+    created_at = models.DateTimeField('date created')
+    updated_at = models.DateTimeField('date updated')
+    content = models.TextField()
+    active = models.BooleanField()
+    tags = models.ManyToManyField('Tag')
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100,
+                            unique=True)
+    articles = models.ManyToManyField('Article')
