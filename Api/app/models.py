2359ffd3e5c9cc0056dacb46cc6f9926022ebf55
Ryan Catlin <ryan.catlin@gmail.com>
2017-08-19 21:22:55 -0400

    Fix many-to-many association on models between Articles and Tags

--- Api/app/models.py
+++ Api/app/models.py
@@ -1,18 +1,16 @@
-from django.db.models import BooleanField, CharField, DateTimeField, ManyToManyField, Model, TextField
+from django.db import models
 
 
-class Article(Model):
-    slug = CharField(max_length=100,
-                     unique=True)
-    title = CharField(max_length=255)
-    created_at = DateTimeField(auto_now_add=True)
-    updated_at = DateTimeField(auto_now=True)
-    content = TextField()
-    active = BooleanField()
-    tags = ManyToManyField('Tag')
-
-
-class Tag(Model):
-    name = CharField(max_length=100,
+class Tag(models.Model):
+    name = models.CharField(max_length=100,
                             unique=True)
-    articles = ManyToManyField('Article')
+
+class Article(models.Model):
+    slug = models.CharField(max_length=100,
+                            unique=True)
+    title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    active = models.BooleanField()
+    tags = models.ManyToManyField(Tag)
